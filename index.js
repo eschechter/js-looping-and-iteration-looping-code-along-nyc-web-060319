8da440fada5e7e5fc08aa26da1b547f6085ecc1a
eschechter <schechter.evan@gmail.com>
2019-07-14 21:07:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(nameArray){
+    const toRet = [];
+    for (let i = 0; i < nameArray.length; i++){
+        toRet.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`);
+    }
+    return toRet;
+}
+
+function countdown(num){
+    while (num >= 0){
+        console.log(num--);
+    }
+}
